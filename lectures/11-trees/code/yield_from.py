b78d411db13674a513407bacfb1f6cc1380a67ca
Sebastián <src402@nyu.edu>
2025-04-07 20:52:39 -0400

    finishing tree notes

---
+++ lectures/11-trees/code/yield_from.py
@@ -0,0 +1,20 @@
+def gen_one():
+    yield 1
+    yield 2
+    yield 3
+
+    
+def gen_two():
+    yield 0
+    
+    # for value in gen_one():
+    #     yield value
+    yield from gen_one()
+    
+    yield 4
+    yield 5
+    
+
+if __name__ == "__main__":
+    for value in gen_two():
+        print(value)
